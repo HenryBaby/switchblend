1927d60f3b50faf8c44105530d8db0cf6f2dd1e1
HenryBaby <ali.dorani@gmail.com>
2024-05-24 14:02:31 +0200

    Added support for individual file/folder to the upload manager.

--- app/upload_manager.py
+++ app/upload_manager.py
@@ -8,12 +8,17 @@ logger = logging.getLogger(__name__)
 ERROR_MESSAGES = {"Errno 113": "Error: Device not found. Is it offline?"}
 
 
-def upload_to_device(ip, port, username, password, local_directory):
+def upload_to_device(ip, port, username, password, local_directory, files):
     try:
         ftp = ftplib.FTP()
         ftp.connect(ip, int(port))
         ftp.login(username, password)
-        upload_directory(ftp, local_directory)
+        for file in files:
+            full_path = os.path.join(local_directory, file)
+            if os.path.isdir(full_path):
+                upload_directory(ftp, full_path, file)
+            else:
+                upload_file(ftp, full_path, file)
         ftp.quit()
         return True, "Upload successful"
     except ftplib.all_errors as e:
@@ -29,19 +34,26 @@ def upload_to_device(ip, port, username, password, local_directory):
         return False, f"Unexpected error: {str(e)}"
 
 
-def upload_directory(ftp, local_directory):
+def upload_file(ftp, local_path, remote_path):
+    with open(local_path, "rb") as f:
+        ftp.storbinary(f"STOR {remote_path}", f)
+        logger.info(f"Uploaded {remote_path}")
+
+
+def upload_directory(ftp, local_directory, remote_directory):
+    try:
+        ftp.mkd(remote_directory)
+    except ftplib.error_perm as e:
+        if not e.args[0].startswith("550"):
+            raise
     for root, dirs, files in os.walk(local_directory):
         for dirname in dirs:
             local_path = os.path.join(root, dirname)
-            relative_path = os.path.relpath(local_path, local_directory)
-            try:
-                ftp.mkd(relative_path)
-            except ftplib.error_perm as e:
-                if not e.args[0].startswith("550"):
-                    raise
+            rel_path = os.path.relpath(local_path, local_directory)
+            remote_path = os.path.join(remote_directory, rel_path)
+            upload_directory(ftp, local_path, remote_path)
         for filename in files:
             local_path = os.path.join(root, filename)
-            relative_path = os.path.relpath(local_path, local_directory)
-            with open(local_path, "rb") as file:
-                ftp.storbinary(f"STOR {relative_path}", file)
-                logger.info(f"Uploaded {relative_path}")
+            rel_path = os.path.relpath(local_path, local_directory)
+            remote_path = os.path.join(remote_directory, rel_path)
+            upload_file(ftp, local_path, remote_path)
